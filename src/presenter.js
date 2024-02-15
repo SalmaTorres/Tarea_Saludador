7c4732447bfef2bb5c70f3a1133b1d7eb301abad
Israel Antezana Rojas <israelantezana@gmail.com>
2024-02-15 09:47:43 -0400

    Se mejora la estrucuta para separar la logica

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-//import sumar from "./sumador";
+import saludar from "./saludar";
 
 const nombre_input = document.querySelector("#nombre");
 const form = document.querySelector("#saludar-form");
@@ -9,5 +9,5 @@ form.addEventListener("submit", (event) => {
 
   const nombre = nombre_input.value;
 
-  div.innerHTML = "<p> Hola " + nombre + "</p>";
+  div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
 });
